6e5fb7d81f9def869ad42b2312fe590e6085f551
yisusturtle <yisusturtle@gmail.com>
2021-11-12 19:03:11 +0100

    Cambio de orden los metodos, pongo main al principio por legibilidad y bajo los metodos abajo

--- TEMA 3/AdivinaElNumeroJesus/src/main/java/jesus/AdivinaAdivinanzaJesus.java
+++ TEMA 3/AdivinaElNumeroJesus/src/main/java/jesus/AdivinaAdivinanzaJesus.java
@@ -1,51 +1,15 @@
 package jesus;
 
-import jdk.swing.interop.SwingInterOpUtils;
-
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class AdivinaAdivinanzaJesus {
-    //METODO QUE PIDE UN NUMERO HASTA QUE SEA CORRECTO, CUANDO ES CORRECTO DEVUELVE EL NUMERO
-    private static int pedirNumero(){
-        Scanner sc = new Scanner(System.in);
-        boolean esCorrecto = true;
-        int numero=0;
-        do{
-            try {
-                numero = sc.nextInt();
-            } catch (InputMismatchException ime){
-                esCorrecto = false;
-            }
-        }while(!esCorrecto);
-
-        return numero;
-    }
-
-    //METODO QUE GENERA UN NUMERO ALEATORIO ENTRE DOS RANGOS QUE SON PASADOS POR PARAMENTROS, DEVUELVE EL NUMERO GENERADO
-    private static int generarAleatorio(int inicioRango, int finalRango){
-        Random random = new Random();
-        int cambioDeOrden;
-
-        //Controlo si el numero inicial es mayor al final, en caso de que lo sean se cambian los valores de las variables entre ellas
-        if(inicioRango>finalRango){
-            cambioDeOrden= inicioRango;
-            inicioRango = finalRango;
-            finalRango = cambioDeOrden;
-        }
-
-        return (random.nextInt(finalRango - inicioRango +1) + inicioRango);
-    }
-
-    //METODO QUE COMPRUEBA SI ES MAYOR O MENOR, DEVUELVE TRUE SI ES MAYOR, FALSE SI ES MENOR
-    private static boolean numeroEsMayor(int primerNumero, int segundoNumero){
-        return (primerNumero>segundoNumero);
-    }
-
-    //---------------------------------------------------------------------------------------------------
-    //              ~~~ METODO MAIN ~~~
-    //--------------------------------------------------------------------------------------------------
+    /*
+        |-------------------------------------------------------------|
+        |              ~~~ METODO MAIN ~~~                            |
+        |-------------------------------------------------------------|
+    */
     public static void main(String[] args) {
         //CONSTANTES
         final int vidas = 3;
@@ -92,5 +56,47 @@ public class AdivinaAdivinanzaJesus {
         }
 
     }
+
+    /*
+        |--------------------------------------------------------------|
+        |               ~~~METODOS CREADOS~~~                          |
+        |--------------------------------------------------------------|
+    */
+    //METODO QUE PIDE UN NUMERO HASTA QUE SEA CORRECTO, CUANDO ES CORRECTO DEVUELVE EL NUMERO
+    private static int pedirNumero(){
+        Scanner sc = new Scanner(System.in);
+        boolean esCorrecto = true;
+        int numero=0;
+        do{
+            try {
+                numero = sc.nextInt();
+            } catch (InputMismatchException ime){
+                esCorrecto = false;
+            }
+        }while(!esCorrecto);
+
+        return numero;
+    }
+
+    //METODO QUE GENERA UN NUMERO ALEATORIO ENTRE DOS RANGOS QUE SON PASADOS POR PARAMENTROS, DEVUELVE EL NUMERO GENERADO
+    private static int generarAleatorio(int inicioRango, int finalRango){
+        Random random = new Random();
+        int cambioDeOrden;
+
+        //Controlo si el numero inicial es mayor al final, en caso de que lo sean se cambian los valores de las variables entre ellas
+        if(inicioRango>finalRango){
+            cambioDeOrden= inicioRango;
+            inicioRango = finalRango;
+            finalRango = cambioDeOrden;
+        }
+
+        return (random.nextInt(finalRango - inicioRango +1) + inicioRango);
+    }
+
+    //METODO QUE COMPRUEBA SI ES MAYOR O MENOR, DEVUELVE TRUE SI ES MAYOR, FALSE SI ES MENOR
+    private static boolean numeroEsMayor(int primerNumero, int segundoNumero){
+        return (primerNumero>segundoNumero);
+    }
+
 
 }
