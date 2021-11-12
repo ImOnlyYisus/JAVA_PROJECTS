ad7b0ba9674fd348bc7c0d5ef8186c3c159d20f0
yisusturtle <yisusturtle@gmail.com>
2021-11-12 14:38:51 +0100

    Ejercicio de Adivinar numeros, modularizado con metodos

---
+++ TEMA 3/AdivinaElNumeroJesus/src/main/java/jesus/AdivinaAdivinanzaJesus.java
@@ -0,0 +1,91 @@
+package jesus;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class AdivinaAdivinanzaJesus {
+    //METODO QUE PIDE UN NUMERO HASTA QUE SEA CORRECTO, CUANDO ES CORRECTO DEVUELVE EL NUMERO
+    private static int pedirNumero(){
+        Scanner sc = new Scanner(System.in);
+        boolean esCorrecto = true;
+        int numero=0;
+        do{
+            try {
+                numero = sc.nextInt();
+            } catch (InputMismatchException ime){
+                esCorrecto = false;
+            }
+        }while(!esCorrecto);
+
+        return numero;
+    }
+
+    //METODO QUE GENERA UN NUMERO ALEATORIO ENTRE DOS RANGOS QUE SON PASADOS POR PARAMENTROS, DEVUELVE EL NUMERO GENERADO
+    private static int generarAleatorio(int inicioRango, int finalRango){
+        Random random = new Random();
+        int cambioDeOrden;
+
+        //Controlo si el numero inicial es mayor al final
+        if(inicioRango>finalRango){
+            cambioDeOrden= inicioRango;
+            inicioRango = finalRango;
+            finalRango = cambioDeOrden;
+        }
+
+        return (random.nextInt(finalRango - inicioRango +1) + inicioRango);
+    }
+
+    //---------------------------------------------------------------------------------------------------
+    //              ~~~ METODO MAIN ~~~
+    //--------------------------------------------------------------------------------------------------
+    public static void main(String[] args) {
+        //CONSTANTES
+        final int vidas = 3;
+
+        //VARIABLES
+        boolean victoria = false;
+
+        //INFORMACION PARA EL JUGADOR
+        System.out.println("Bienvenido al juego");
+        System.out.println("Debes de adivinar el numero...");
+        System.out.println("Tienes 3 oportunidades.");
+
+        //RANGOS DE NUMEROS SOLICITADOS
+        System.out.println("Dime el rango de numeros en el que quieres adivinar");
+        System.out.println("Desde:");
+        int inicioRango =  pedirNumero();
+        System.out.println("Hasta:");
+        int finalRango = pedirNumero();
+
+        //GENERO NUMERO ALEATORIO
+        int numeroAleatorio= generarAleatorio(inicioRango,finalRango);
+
+        //LOGICA DEL JUEGO: 3 VIDAS, EN EL CASO DE QUE LO ACIERTE SALE Y GUARDA EN UNA VARIABLE SI HA GANADO, EN EL OTRO CASO PIERDE
+        for(int i=1; i <= vidas; i++){
+
+            System.out.println("Intento numero [" + i + "]: ");
+            int numeroSugerido = pedirNumero();
+
+            if(numeroAleatorio > numeroSugerido){
+                System.out.println((i<3) ? "El numero es mayor" : "Se acabo el juego");
+            } else if(numeroAleatorio < numeroSugerido){
+                System.out.println((i<3) ? "El numero es menor" : "Se acabo el juego");
+            } else{
+                victoria = true;
+                break;
+            }
+        }
+
+        //COMPRUEBA SI HA GANADO O PERDIDO
+        if(victoria){
+            System.out.println("Enhorabuena el numero era ["+ numeroAleatorio +"]");
+        } else{
+            System.out.println("Lo siento has perdido el numero era ["+ numeroAleatorio +"]");
+        }
+
+    }
+
+}
