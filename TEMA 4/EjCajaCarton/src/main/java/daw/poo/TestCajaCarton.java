8e43450809ae9218fc9a245d69c1042cd7265946
yisusturtle <yisusturtle@gmail.com>
2021-12-16 22:48:55 +0100

    Caja de carton relacion b

---
+++ TEMA 4/EjCajaCarton/src/main/java/daw/poo/TestCajaCarton.java
@@ -0,0 +1,16 @@
+package daw.poo;
+
+import daw.poo.CajaCarton;
+
+public class TestCajaCarton {
+    public static void main(String[] args) {
+        CajaCarton cajaGrande = new CajaCarton(250,400,500,200);
+        CajaCarton cajaChica = new CajaCarton(25,30,15,17);
+
+        System.out.println("cajaChica"+cajaChica.toString());
+        System.out.println("cajaGrande"+cajaGrande.toString());
+
+        cajaChica.setPeso(1200);
+        System.out.println(cajaChica.getPeso());
+    }
+}
