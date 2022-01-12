885f21c7ea2f49336736bd5399be47a2eb250bea
yisusturtle <yisusturtle@gmail.com>
2022-01-12 14:05:33 +0100

    Dni

--- TEMA 4/EjercicioNIF/src/main/java/jesus/Prueba.java
+++ TEMA 4/EjercicioNIF/src/main/java/jesus/Prueba.java
@@ -2,8 +2,8 @@ package jesus;
 
 public class Prueba {
     public static void main(String[] args) {
-        Nif nifPersona1= new Nif("76646220");
-        Nif nifPersona2= new Nif("12345678");
+        Nif nifPersona1= new Nif("12345467");
+        Nif nifPersona2= new Nif("12345608");
         System.out.println(nifPersona1);
         System.out.println(nifPersona2);
     }
