17f9594be226e984fd8acbf95ccd3d574f19f1a8
yisusturtle <yisusturtle@gmail.com>
2022-01-14 10:47:39 +0100

    Pruebo y modifico metodos fecha siguiente y anterior

--- TEMA 4/EjercicioFecha/src/main/java/jesus/Fecha.java
+++ TEMA 4/EjercicioFecha/src/main/java/jesus/Fecha.java
@@ -14,15 +14,15 @@ public class Fecha {
     private int anio;
 
     //Contructores
-    public Fecha(){ //Por defecto
-        this.dia=1;
-        this.mes=1;
-        this.anio=2022;
+    public Fecha() { //Por defecto
+        this.dia = 1;
+        this.mes = 1;
+        this.anio = 2022;
     }
 
     public Fecha(int dia, int mes, int anio) { //Parametrizado
         //Comprobar fecha
-        if(!comprobarFecha(dia,mes,anio)){
+        if (!comprobarFecha(dia, mes, anio)) {
             throw new IllegalArgumentException("Los parametros introducidos son invalidos");
         }
 
@@ -45,112 +45,108 @@ public class Fecha {
     }
 
     public void setDia(int dia) {
-        if(comprobarFecha(dia,this.mes,this.anio)){
+        if (comprobarFecha(dia, this.mes, this.anio)) {
             this.dia = dia;
         }
     }
 
     public void setMes(int mes) {
-        if(comprobarFecha(this.dia,mes,this.anio)){
+        if (comprobarFecha(this.dia, mes, this.anio)) {
             this.mes = mes;
         }
     }
 
     public void setAnio(int anio) {
-        if(comprobarFecha(this.dia,this.mes,anio)){
+        if (comprobarFecha(this.dia, this.mes, anio)) {
             this.anio = anio;
         }
     }
 
     //Metodos
-    public boolean comprobarFecha(int dia, int mes, int anio){
-        boolean verificarFecha= true;
-        try{
-            LocalDate fecha= LocalDate.of(anio,mes,dia);
-        }catch (DateTimeException dte){
-            verificarFecha=!verificarFecha;
+    public boolean comprobarFecha(int dia, int mes, int anio) {
+        boolean verificarFecha = true;
+        try {
+            LocalDate fecha = LocalDate.of(anio, mes, dia);
+        } catch (DateTimeException dte) {
+            verificarFecha = !verificarFecha;
         }
 
         return verificarFecha;
     }
 
-    public boolean bisiesto(){
-        return LocalDate.of(this.anio,this.mes,this.dia).isLeapYear();
+    public boolean bisiesto() {
+        return LocalDate.of(this.anio, this.mes, this.dia).isLeapYear();
     }
 
-    public int diasMes(){
-        return LocalDate.of(this.anio,this.mes,this.dia).getDayOfMonth();
+    public int diasMes() {
+        return LocalDate.of(this.anio, this.mes, this.dia).getDayOfMonth();
     }
 
-    public String mostrarFechaCorta(){
-        LocalDate fecha= LocalDate.of(this.anio,this.mes,this.dia);
-        DateTimeFormatter formateoFecha= DateTimeFormatter.ofPattern("dd-mm-yyyy");
+    public String mostrarFechaCorta() {
+        LocalDate fecha = LocalDate.of(this.anio, this.mes, this.dia);
+        DateTimeFormatter formateoFecha = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         return formateoFecha.format(fecha);
     }
 
-    public String mostrarFechaLarga(){
-        Locale spanishLocale=new Locale("es", "ES");
-        LocalDate fecha= LocalDate.of(this.anio,this.mes,this.dia);
-        DateTimeFormatter formateoFecha= DateTimeFormatter.ofPattern("EEEE d 'de' MMMM 'del' yyyy", spanishLocale);
+    public String mostrarFechaLarga() {
+        Locale spanishLocale = new Locale("es", "ES");
+        LocalDate fecha = LocalDate.of(this.anio, this.mes, this.dia);
+        DateTimeFormatter formateoFecha = DateTimeFormatter.ofPattern("EEEE d 'de' MMMM 'del' yyyy", spanishLocale);
         return formateoFecha.format(fecha);
     }
 
-//    public diaSemana(){
-//
-//    }
-
-    public void siguiente(){
-        LocalDate fecha= LocalDate.of(this.anio,this.mes,this.dia);
-        fecha.plusDays(1);//Pasar dia
-
-        this.dia=fecha.getDayOfMonth();
-        this.mes=fecha.getMonthValue();
-        this.anio=fecha.getYear();
+    public String diaSemana() {
+        LocalDate fecha = LocalDate.of(this.anio, this.mes, this.dia);
+        return ("" + fecha.getDayOfWeek());
     }
 
-    public void anterior(){
-        LocalDate fecha= LocalDate.of(this.anio,this.mes,this.dia);
-        fecha.plusDays(-1);//Pasar dia
+    public void siguiente() {
+        LocalDate fecha = LocalDate.of(this.anio, this.mes, this.dia);
+        fecha =fecha.plusDays(1);//Pasar dia
 
-        this.dia=fecha.getDayOfMonth();
-        this.mes=fecha.getMonthValue();
-        this.anio=fecha.getYear();
+        this.dia = fecha.getDayOfMonth();
+        this.mes = fecha.getMonthValue();
+        this.anio = fecha.getYear();
     }
 
-    public Fecha copia(){
+    public void anterior() {
+        LocalDate fecha = LocalDate.of(this.anio, this.mes, this.dia);
+        fecha=fecha.plusDays(-1);//Pasar dia
+
+        this.dia = fecha.getDayOfMonth();
+        this.mes = fecha.getMonthValue();
+        this.anio = fecha.getYear();
+    }
+
+    public Fecha copia() {
         Fecha fechaCopia;
-        fechaCopia= this;
+        fechaCopia = this;
         return fechaCopia;
     }
 
-    public boolean igualQue(Fecha fechaPasada){
-        LocalDate fecha= LocalDate.of(this.anio,this.mes,this.dia);
-        ChronoLocalDate pasada= LocalDate.of(fechaPasada.getAnio(),fechaPasada.getMes(),fechaPasada.getDia());
+    public boolean igualQue(Fecha fechaPasada) {
+        LocalDate fecha = LocalDate.of(this.anio, this.mes, this.dia);
+        ChronoLocalDate pasada = LocalDate.of(fechaPasada.getAnio(), fechaPasada.getMes(), fechaPasada.getDia());
         return (fecha.isEqual(pasada));
     }
 
-    public boolean mayorQue(Fecha fechaPasada){
-        LocalDate fecha= LocalDate.of(this.anio,this.mes,this.dia);
-        ChronoLocalDate pasada= LocalDate.of(fechaPasada.getAnio(),fechaPasada.getMes(),fechaPasada.getDia());
+    public boolean mayorQue(Fecha fechaPasada) {
+        LocalDate fecha = LocalDate.of(this.anio, this.mes, this.dia);
+        ChronoLocalDate pasada = LocalDate.of(fechaPasada.getAnio(), fechaPasada.getMes(), fechaPasada.getDia());
         return (fecha.isAfter(pasada));
     }
 
-    public boolean menorQue(Fecha fechaPasada){
-        LocalDate fecha= LocalDate.of(this.anio,this.mes,this.dia);
-        ChronoLocalDate pasada= LocalDate.of(fechaPasada.getAnio(),fechaPasada.getMes(),fechaPasada.getDia());
+    public boolean menorQue(Fecha fechaPasada) {
+        LocalDate fecha = LocalDate.of(this.anio, this.mes, this.dia);
+        ChronoLocalDate pasada = LocalDate.of(fechaPasada.getAnio(), fechaPasada.getMes(), fechaPasada.getDia());
         return (fecha.isBefore(pasada));
     }
 
-    public static long diasEntreFechas(Fecha inicial, Fecha fin){
-        LocalDate incio=LocalDate.of(inicial.getAnio(),inicial.getMes(),inicial.getDia());
-        LocalDate ultima=LocalDate.of(fin.getAnio(),fin.getMes(),fin.getDia());
+    public static long diasEntreFechas(Fecha inicial, Fecha fin) {
+        LocalDate incio = LocalDate.of(inicial.getAnio(), inicial.getMes(), inicial.getDia());
+        LocalDate ultima = LocalDate.of(fin.getAnio(), fin.getMes(), fin.getDia());
 
-        return ChronoUnit.DAYS.between(incio,ultima);
+        return ChronoUnit.DAYS.between(incio, ultima);
     }
 
-    //leerDia(): leerá un entero por teclado hasta que se encuentre en el rango 1 – 31 y lo devolverá.
-    //
-    //leerMes(): leerá un entero por teclado hasta que se encuentre en el rango 1 – 12 y lo devolverá.
-    //
-    //leerAnio(): leerá un entero por teclado hasta que se encuentre en el rango 1900 – 2100 y lo devolverá.
 }
--- TEMA 4/EjercicioFecha/src/main/java/jesus/Prueba.java
+++ TEMA 4/EjercicioFecha/src/main/java/jesus/Prueba.java
@@ -4,10 +4,24 @@ import java.time.format.DateTimeFormatter;
 
 public class Prueba {
     public static void main(String[] args) {
-        Fecha fecha= new Fecha();
+        Utilidades utilidades= new Utilidades();
+        Fecha fecha= new Fecha(20,1,2005);
 
+        System.out.println(fecha.bisiesto());
+        System.out.println(fecha.diasMes());
+        System.out.println(fecha.mostrarFechaCorta());
         System.out.println(fecha.mostrarFechaLarga());
+        System.out.println(fecha.diaSemana());
+        fecha.siguiente();
+        System.out.println(fecha.mostrarFechaCorta());
+        fecha.anterior();
+        System.out.println(fecha.mostrarFechaCorta());
 
-        System.out.println(fecha+" "+fecha.copia());
+        Fecha fecha2= new Fecha(10,5,2021);
+        System.out.println(fecha.igualQue(fecha2));
+        System.out.println(fecha.mayorQue(fecha2));
+        System.out.println(fecha.menorQue(fecha2));
+
+        System.out.println(Fecha.diasEntreFechas(fecha,fecha2));
     }
 }
