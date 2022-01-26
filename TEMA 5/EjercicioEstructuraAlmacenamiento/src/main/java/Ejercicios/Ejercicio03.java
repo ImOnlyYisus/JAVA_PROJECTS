df17c7f28139e871511217c1b45ed04c713622a0
yisusturtle <yisusturtle@gmail.com>
2022-01-26 14:25:27 +0100

    Ejercicio 3 API Arrays

---
+++ TEMA 5/EjercicioEstructuraAlmacenamiento/src/main/java/Ejercicios/Ejercicio03.java
@@ -0,0 +1,38 @@
+package Ejercicios;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Ejercicio03 {
+    public static void main(String[] args) {
+        Random rd= new Random();
+
+        double[] primerArray= new double[20];
+        double[] segundoArray = new double[20];
+
+        Arrays.fill(primerArray, 7.5);
+        Arrays.fill(segundoArray,7.5);
+
+        double[] copiaArray = Arrays.copyOf(primerArray, primerArray.length);
+        System.out.println(primerArray.hashCode());
+        for (int i = 0; i <primerArray.length ; i++) {
+            System.out.println(primerArray[i]);
+        }
+        System.out.println(segundoArray.hashCode());
+        for (int i = 0; i <segundoArray.length ; i++) {
+            System.out.println(segundoArray[i]);
+        }
+        System.out.println(copiaArray.hashCode());
+        for (int i = 0; i <copiaArray.length ; i++) {
+            System.out.println(copiaArray[i]);
+        }
+        System.out.println(Arrays.equals(primerArray,copiaArray));
+
+        primerArray[rd.nextInt(primerArray.length)]=6.3;
+        for (int i = 0; i <primerArray.length ; i++) {
+            System.out.println(primerArray[i]);
+        }
+        System.out.println(Arrays.equals(primerArray,copiaArray));
+
+    }
+}
