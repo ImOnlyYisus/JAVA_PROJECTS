55f901c37afc78b971d1ac28ce15fc14212635e3
yisusturtle <yisusturtle@gmail.com>
2022-02-22 13:46:24 +0100

    Vecinas corregido

--- TEMA 5/Matrices/src/main/java/matricespruebas/Main.java
+++ TEMA 5/Matrices/src/main/java/matricespruebas/Main.java
@@ -10,13 +10,13 @@ public class Main {
                 {8, 9, 1, 2},
                 {3, 4, 5, 6}
         };
-//        recorrerFila(2,matriz);
-//        recorrerColumna(0,matriz);
-        recorrerVecinas(0, 0, matriz);
+        recorrerFila(2,matriz);
+        recorrerColumna(0,matriz);
+        recorrerVecinas(3, 0, matriz);
     }
 
     public static void recorrerFila(int x, int[][] matriz) {
-        if (matriz.length >= x && x >= 0) {
+        if (matriz.length > x && x >= 0) {
             for (int i = 0; i < matriz[x].length; i++) {
                 System.out.print(matriz[x][i] + "\t");
             }
@@ -26,14 +26,14 @@ public class Main {
 
     public static void recorrerColumna(int x, int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
-            if (matriz[i].length >= x && x >= 0) {
+            if (matriz[i].length > x && x >= 0) {
                 System.out.println(matriz[i][x]);
             }
         }
     }
 
     public static void recorrerVecinas(int i, int j, int[][] matriz) {
-        if ((i >= 0 && j >= 0) && matriz.length >= i && matriz[i].length >= j) {
+        if ((i >= 0 && j >= 0) && matriz.length > i && matriz[i].length > j) {
             int[] minimoCoordenada = {i - 1, j - 1};
             int[] maximoCoordenada = {i + 1, j + 1};
 
