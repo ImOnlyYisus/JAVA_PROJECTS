64b72681fe8f3a8c174167cf5f2f48a7d20d55ec
yisusturtle <yisusturtle@gmail.com>
2022-03-31 16:17:45 +0200

    getter implementado

--- INTERFACES_GUI/tarea4GUIJesusNarbona/src/main/java/calculadora/PanelLCD.java
+++ INTERFACES_GUI/tarea4GUIJesusNarbona/src/main/java/calculadora/PanelLCD.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelLCD extends JPanel {
-    private JLabel calculos = new JLabel("ejemplo");
+    private JLabel calculos = new JLabel("0");
 
     public PanelLCD() {
         initComponets();
@@ -14,8 +14,12 @@ public class PanelLCD extends JPanel {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.setBackground(Color.BLACK);
 
-        calculos.setFont(new Font("Dialog", 1, 29));
+        calculos.setFont(new Font(Font.MONOSPACED,1,29));
         calculos.setForeground(Color.WHITE);
         this.add(calculos);
+    }
+
+    public JLabel getCalculos() {
+        return calculos;
     }
 }
