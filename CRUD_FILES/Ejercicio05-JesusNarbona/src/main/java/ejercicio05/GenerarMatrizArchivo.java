15199cdfec87ab7e755cc0c399702813d1fad585
yisusturtle <yisusturtle@gmail.com>
2022-04-20 13:55:33 +0200

    Arreglo del ejercicio 5

--- CRUD_FILES/Ejercicio05-JesusNarbona/src/main/java/ejercicio05/GenerarMatrizArchivo.java
+++ CRUD_FILES/Ejercicio05-JesusNarbona/src/main/java/ejercicio05/GenerarMatrizArchivo.java
@@ -2,44 +2,31 @@ package ejercicio05;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GenerarMatrizArchivo {
-    private static int[][] matriz;
-
-    public static void imprimirMatriz(){
-        for (int i = 0; i <matriz.length ; i++) {
-            for (int j = 0; j <matriz[i].length ; j++) {
-                System.out.println(matriz[i][j]);
-            }
-        }
-    }
-
     public static void main(String[] args) {
+        //Ruta del archivo
         String route = "matriz.txt";
-        String matrizRead ="";
-        ArrayList<String[]> objetosDeMatriz = new ArrayList<>();
 
+        String matrizRead ="";
+        int suma=0;
+
+        //Leer el archivo
         try (Scanner sc = new Scanner(new FileReader(route))){
             while(sc.hasNextLine()){
                 matrizRead=sc.nextLine();
                 String[] splitData = matrizRead.split("\\s");
-                objetosDeMatriz.add(splitData);
+                for (int i = 0; i <splitData.length ; i++) {
+                    suma+=Integer.parseInt(splitData[i]);
+                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
-        matriz= new int[objetosDeMatriz.size()][objetosDeMatriz.size()];
-        for (int i = 0; i <objetosDeMatriz.size() ; i++) {
-            for (int j = 0; j <objetosDeMatriz.get(i).length ; j++) {
-                matriz[i][j]=Integer.parseInt(objetosDeMatriz.get(i)[j]);
-            }
-        }
-
-        GenerarMatrizArchivo.imprimirMatriz();
+        System.out.println(suma);
 
     }
 
