0441a486b769caee6248007bdad500ef010049b7
yisusturtle <yisusturtle@gmail.com>
2022-04-29 14:02:06 +0200

    Ejercicio 7.1 terminado, con stream

--- CRUD_FILES/Tarea7AJesusNarbona/src/main/java/tarea7a/Main.java
+++ CRUD_FILES/Tarea7AJesusNarbona/src/main/java/tarea7a/Main.java
@@ -2,6 +2,9 @@ package tarea7a;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,16 +18,35 @@ public class Main {
         EscrituraProfesDept.escritura(empleados);
         EscrituraEmp100dias.escritura(empleados);
 
-        //Metodos de la clase Utils
-        System.out.println(Utils.buscarEmpleadoNombre(empleados, "Mario"));
+        //Metodos de stream
+        //A partir de una lista de empleados y un nombre, indique si hay algún empleado con ese nombre.
+        boolean estaEmpleado = empleados.stream()
+                .anyMatch(empleado -> empleado.getNombre().equalsIgnoreCase("Ana"));
+        System.out.println(estaEmpleado);
 
-        ArrayList<String> listaApellidosDNI = Utils.listaAlfabeticaApellidoNif(empleados,'c');
-        listaApellidosDNI.forEach(System.out::println);
+        //A partir de una lista de empleados y un nombre de departamento, indique el número de empleados Coordinadores de ese departamento.
+        long numeroEmpleados = empleados.stream()
+                .filter(empleado -> empleado.getPuesto().equalsIgnoreCase("prueba"))
+                .filter(empleado -> empleado.isCoordinador())
+                .count();
+        System.out.println(numeroEmpleados);
 
-        int numeroCoordinadores = Utils.empleadosCoordinadoresDept(empleados, "prueba");
-        System.out.println("Numeros de coordinadores en prueba --> " + numeroCoordinadores);
+        //A partir de una lista de empleados y una letra del NIF (char), obtener una nueva lista ordenada alfabéticamente SOLO con los apellidos
+        // de los empleados cuyo NIF contenga esa letra.
+        List<String> listaOrdenadaApellidos = empleados.stream()
+                .filter(empleado -> empleado.getDni().contains(String.valueOf('c')))
+                .map(Empleado::getApellido)
+                .sorted((empleado1, empleado2) -> empleado1.compareToIgnoreCase(empleado2))
+                .collect(Collectors.toList());
+        listaOrdenadaApellidos.forEach(System.out::println);
 
-        ArrayList<String> listaFecha = Utils.obtenerListaFecha(empleados, LocalDate.of(2020,9,1));
-        listaFecha.forEach(System.out::println);
+        //A partir de una lista de empleados y una fecha, obtener una nueva lista con los NIF (ordenados de forma inversa) de todos los empleados cuya
+        //toma de posesión coincida con esa fecha.
+        List<String> listaInversaDni = empleados.stream()
+                .filter(empleado -> empleado.getFechaToma().equals(LocalDate.of(10,1,1995)))
+                .map(Empleado::getDni)
+                .sorted((empleado1, empleado2) -> empleado2.compareToIgnoreCase(empleado1))
+                .collect(Collectors.toList());
+        listaInversaDni.forEach(System.out::println);
     }
 }
